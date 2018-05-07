d1be3f45b1fe7983b3558b8b6eb840cf7116e5b4
Wilhelm <nightmare.v.94@gmail.com>
2018-05-07 17:55:34 +0500

    Добавлена панель с ожиданием сканирования

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,33 +17,8 @@ namespace WpfApp
 			_instance = this;
 			_mainContent = new MainContent();
 			RootContent.Content = _mainContent;
-//
-//			for (var i = 0; i < 10; i++)
-//			{
-//				Contract.Repository.AddAndSave(new BsonDocument
-//				{
-//					{"Number", GetRandomString()},
-//					{"BoxId", "5ae01249c019612a845a32ee"},
-//					{"ClientFirstName", GetRandomString()},
-//					{"ClientLastName", GetRandomString()},
-//					{"ClientPatronymic", GetRandomString()},
-//					{"PhoneNumber", GetRandomString()},
-//					{"LoanId", GetRandomString()},
-//					{"PrefixOfPlace", GetRandomString()},
-//					{"ContractDate", DateTime.Now }
-//				});
-//			}
 
 			showMsg();
-		}
-
-		private static Random r = new Random();
-
-		static string GetRandomString()
-		{
-			const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-			return new string(Enumerable.Repeat(chars, 14)
-				.Select(s => s[r.Next(s.Length)]).ToArray());
 		}
 
 		public async void showMsg()
